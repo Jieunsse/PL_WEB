c134e3065e4474cb040d7d360ec48fcd1c2d36f3
jieunsse <jieunsse@gmail.com>
2025-05-29 11:42:23 +0900

    fix : tsconfigPaths add

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
-import './normalize.css';
+import 'normalize.css';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { Router } from '@src/app/routes/Router.tsx';
+import { Router } from '@app/routes/Router.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
